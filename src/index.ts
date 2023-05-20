82acdf771162baefa0cf9df89ca45c71fe8bec95
Jordi Calles <jordicalles1989@gmail.com>
2023-05-20 15:22:41 +0200

    Start server and connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-const a = 3;
+import "./server/loadEnvironments.js";
+import createDebug from "debug";
+import chalk from "chalk";
+import connectToDatabase from "./connectToDatabase.js";
+import app from "./server/index.js";
 
-export default a;
+const debug = createDebug("user-pirates-api:root");
+
+const port = process.env.PORT ?? 4000;
+const mongoDbConnection = process.env.MONGO_DB_CONNECTION;
+
+if (!mongoDbConnection) {
+  debug(chalk.red("Missing the environment variables!"));
+  process.exit(1);
+}
+
+app.listen(port, () => {
+  debug(chalk.green(`Listening on http://localhost:${port}`));
+});
+
+try {
+  await connectToDatabase(mongoDbConnection);
+  debug(chalk.green("Connection to the database succesful"));
+} catch {
+  debug(chalk.red("Error while connecting to the database"));
+}
